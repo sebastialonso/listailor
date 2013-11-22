13ee1558767afebc015eaacabbea61935d69bd9a
sebastialonso <sebagonz91@gmail.com>
2013-11-22 17:28:04 +0100

    Deleted migration with repeated instructions

--- db/migrate/20131121002420_add_user_id_to_list.rb
+++ db/migrate/20131121002420_add_user_id_to_list.rb
@@ -1,5 +1,7 @@
 class AddUserIdToList < ActiveRecord::Migration
   def change
     add_column :lists, :user_id, :integer
+    add_column :lists, :name, :string
+    add_column :lists, :description, :text
   end
 end
--- db/migrate/20131121010905_add_name_and_description_to_list.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndDescriptionToList < ActiveRecord::Migration
-  def change
-    add_column :lists, :name, :string
-    add_column :lists, :description, :text
-  end
-end
