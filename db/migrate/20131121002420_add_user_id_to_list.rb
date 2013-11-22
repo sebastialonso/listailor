4a12040be15ae1b2a7c4aa9e8af6ea7002ea35e4
sebastialonso <sebagonz91@gmail.com>
2013-11-22 17:07:06 +0100

    Modified one migration (repeated instruction: name on list)

--- db/migrate/20131121002420_add_user_id_to_list.rb
+++ db/migrate/20131121002420_add_user_id_to_list.rb
@@ -1,7 +1,5 @@
 class AddUserIdToList < ActiveRecord::Migration
   def change
     add_column :lists, :user_id, :integer
-    add_column :lists, :name, :string
-    add_column :lists, :description, :text
   end
 end
