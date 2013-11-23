78bef1b60677977a943ae67cea7ad084b44ac1a5
sebastialonso <sebagonz91@gmail.com>
2013-11-24 00:30:07 +0100

    Flash messages working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,9 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
     logger.debug "#{request.env["omniauth.auth"]}"
     if @user.persisted?
-      flash[:notice] = "Loggeado a través de Google!"
+      flash[:notice] = 'Loggeado con Google+!'
       sign_in_and_redirect @user, :event => :authentication
     else
+      #This never happens
       flash[:notice] = "Estamos casi! Danos una contraseña para ti!"
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
